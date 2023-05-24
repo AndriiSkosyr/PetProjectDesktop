e658ac1196db425548f7d5fe11b53c386e1661f4
AndriiSkosyr <andrewskosyr@gmail.com>
2023-05-24 23:13:32 +0300

    AudioToTextApp v. 0.3 whisper model works and format converts

--- AudioToTextApp/formatConverter.py
+++ AudioToTextApp/formatConverter.py
@@ -1,6 +1,8 @@
 from pydub import AudioSegment
 
-m4a_file = 'test_m4a.m4a'
-wav_filename = r"D:\test_wav.wav"
-track = AudioSegment.from_file(m4a_file, format='m4a')
-file_handle = track.export(wav_filename, format='wav')
+
+def formatting(m4a_file):
+    wav_filename = r"D:\test_wav.wav"
+    track = AudioSegment.from_file(m4a_file, format='m4a')
+    file_handle = track.export(wav_filename, format='wav')
+    print(file_handle)
--- AudioToTextApp/whisper.py
+++ AudioToTextApp/whisper.py
@@ -3,12 +3,14 @@ import torch
 import soundfile as sf
 import os
 import torchaudio
+import formatConverter
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 whisper_model_name = "openai/whisper-small"
 whisper_processor = WhisperProcessor.from_pretrained(whisper_model_name)
 whisper_model = WhisperForConditionalGeneration.from_pretrained(whisper_model_name).to(device)
+
 
 def load_audio(audio_path):
   """Load the audio file & convert to 16,000 sampling rate"""
@@ -17,6 +19,7 @@ def load_audio(audio_path):
   resampler = torchaudio.transforms.Resample(sr, 16000)
   speech = resampler(speech)
   return speech.squeeze()
+
 
 def get_transcription_whisper(audio_path, model, processor, language="english", skip_special_tokens=True):
   # resample from whatever the audio sampling rate to 16000
@@ -30,5 +33,6 @@ def get_transcription_whisper(audio_path, model, processor, language="english", 
   # decode the IDs to text
   transcription = processor.batch_decode(predicted_ids, skip_special_tokens=skip_special_tokens)[0]
   return transcription
+
 
 print(get_transcription_whisper("test_wav.wav", whisper_model, whisper_processor))
